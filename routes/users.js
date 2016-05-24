b2e6c19b5ed7a36ed7dab262304f60b0e9646aa8
Dany <dannyleduc16@hotmail.com>
2016-05-24 15:33:13 +0200

    Passport Login Authentication

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest:'./uploads'});
 var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 /*Same as /users/ because of path in app.js*/
@@ -14,9 +16,48 @@ router.get('/register', function(req, res, next) {
   res.render('register',{title:'Register'});
 });
 
+
 router.get('/login', function(req, res, next) {
   res.render('login',{title:'Login'});
 });
+
+
+router.post('/login',
+  passport.authenticate('local',{failureRedirect:'/users/login',failureFlash:'Invalid username or password'}),
+  function(req, res) {
+  	console.log('-----Login');
+  	req.flash('success','You are now logged in');
+    res.redirect('/');
+  });
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username,password,done){
+		User.getUserByUsername(username,function(err,user){
+	if(err) throw err;
+	if(!user){
+	return done(null,false,{message: 'Unknown User'});
+		}
+	User.comparePassword(password,user.password,function(err,isMatch){
+		if(err) return done(err);
+		if(isMatch){
+			return done(null,user);
+		}else {
+			return done(null,false,{message:'Invalid Password'});
+		}
+		});
+	});
+}));
+
+
 
 router.post('/register', upload.single('profileimage'),function(req, res, next) {
   var name=req.body.name;
